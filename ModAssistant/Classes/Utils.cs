c832ab2311638026a38615ffcc2d10d31e292873
MegaMagicPower <no@gmail.com>
2019-07-28 15:57:07 -0400

    Removed piracy check

--- ModAssistant/Classes/Utils.cs
+++ ModAssistant/Classes/Utils.cs
@@ -350,15 +350,6 @@ namespace ModAssistant
 
         public static bool isVoid()
         {
-            string directory = App.BeatSaberInstallDirectory;
-
-            if (File.Exists(Path.Combine(directory, "IGG-GAMES.COM.url")) ||
-                File.Exists(Path.Combine(directory, "SmartSteamEmu.ini")) ||
-                File.Exists(Path.Combine(directory, "GAMESTORRENT.CO.url")) ||
-                File.Exists(Path.Combine(directory, "Beat Saber_Data", "Plugins", "BSteam crack.dll")) ||
-                File.Exists(Path.Combine(directory, "Beat Saber_Data", "Plugins", "HUHUVR_steam_api64.dll")) ||
-                Directory.GetFiles(Path.Combine(directory, "Beat Saber_Data", "Plugins"), "*.ini", SearchOption.TopDirectoryOnly).Length > 0)
-                return true;
             return false;
         }
 
